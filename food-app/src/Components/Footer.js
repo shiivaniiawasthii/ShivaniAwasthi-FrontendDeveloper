4a1a42d6d99a75e5f06a6288f094748846a84600
shiivaniiawasthii <shvaniawsthi001@gmail.com>
2024-04-10 13:45:46 +0530

    Footer component is pushed

--- food-app/src/Components/Footer.js
+++ food-app/src/Components/Footer.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="bg-black text-gray-400 p-10 mt-12 ">
+      <div className="ml-40">
+        <div className="flex  -ml-4">
+          <img className="w-12 h-12" src="/footer-logo.jpg" alt="logo"></img>
+          <div>
+            {" "}
+            <h1 className="font-bold text-2xl text-white mt-1">Swiggy </h1>
+          </div>
+        </div>
+
+        <p>© 2024 Bundl Technologies Pvt. Ltd</p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
